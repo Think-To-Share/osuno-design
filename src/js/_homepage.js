9b92755f4150f7d3510966704671cd32a64bf348
Tulika Das <tulikadas1029@gmail.com>
2022-02-01 20:42:49 +0530

    banner and studio and responsive section added

--- src/js/_homepage.js
+++ src/js/_homepage.js
@@ -3,13 +3,36 @@ import { tns } from 'tiny-slider/src/tiny-slider'
 if (document.querySelector('.featured-section')) {
     tns({
         container: '.featured-section .slides',
-        items: 6,
+        items: 2,
         nav: false,
         gutter:20,
         autoplay: true,
-        controls:true,
+        controls:false,
         autoplayTimeout:3000,
         autoplayButtonOutput: false,
-        controlsText:['<i class="fas fa-chevron-left"></i>' , '<i class="fas fa-chevron-right"></i>']
+        controlsText:['<i class="fas fa-chevron-left"></i>' , '<i class="fas fa-chevron-right"></i>'],
+        responsive: {
+            768:{
+                items: 3,
+                controls:false,
+            },
+            1200:{
+            items: 6,
+            controls:true,
+            },
+        }
+    })
+}
+
+
+if (document.querySelector('.osuno-hero-section')) {
+    tns({
+        container: '.osuno-hero-section .slides',
+        items: 1,
+        nav: true,
+        autoplay: true,
+        controls:false,
+        autoplayTimeout:3000,
+        autoplayButtonOutput: false,
     })
 }
